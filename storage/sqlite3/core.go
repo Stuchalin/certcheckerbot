79e304b49b5d9f85e4ffd68a67fb0b89f9f6cd15
Denis Stuchalin <stuchalin.d@gmail.com>
2022-07-19 01:12:40 +0300

    refactor errors

--- storage/sqlite3/core.go
+++ storage/sqlite3/core.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"errors"
 	_ "github.com/mattn/go-sqlite3"
-	"log"
 	"os"
 )
 
@@ -13,25 +12,30 @@ func NewDB(databaseName string) (*sql.DB, error) {
 	if _, err := os.Stat("/path/to/whatever"); errors.Is(err, os.ErrNotExist) {
 		file, err := os.Create(databaseName)
 		if err != nil {
-			log.Fatal(err)
 			return nil, err
 		}
-		file.Close()
+		err = file.Close()
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	db, err := sql.Open("sqlite3", databaseName)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	_, err = MigrateToActualVersion(db)
+	if err != nil {
 		return nil, err
 	}
 
 	return db, nil
 }
 
-func CloseConnection(db sql.DB) {
-	db.Close()
+func CloseConnection(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		return
+	}
 }
-
-//func migrateToActualVersion(db *sql.DB) (bool, error) {
-//
-//}
