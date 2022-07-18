c28073d95ea716ea40e859842e0b5f64830aa2d2
Denis Stuchalin <stuchalin.d@gmail.com>
2022-07-19 01:30:31 +0300

    refactor

--- storage/controller.go
+++ storage/controller.go
@@ -3,8 +3,8 @@ package storage
 type UsersConfig interface {
 	AddUser(user *User) (int, error)
 	GetUserById(id int) (User, error)
-	GetUserByTGId(tgId string) (User, error)
-	GetUserByName(name string) (User, error)
+	GetUserByTGId(tgId string) (*User, error)
+	GetUserByName(name string) (*User, error)
 	RemoveUser(user *User) (bool, error)
 	UpdateUserInfo(user *User) (bool, error)
 
