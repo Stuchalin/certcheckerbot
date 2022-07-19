8e8f39ec5bfa94ee877e6d2d11f6576420853787
Denis Stuchalin <stuchalin.d@gmail.com>
2022-07-19 21:05:35 +0300

    sqlite3 controller realization and tests

--- storage/sqlite3/controller.go
+++ storage/sqlite3/controller.go
@@ -6,12 +6,15 @@ import (
 	"fmt"
 )
 
+//Sqlite3Controller controller for sqlite3 database
 type Sqlite3Controller struct {
 	Connection *sql.DB
 }
 
-func NewController(databaseName string) (*Sqlite3Controller, error) {
-	db, err := NewDB(databaseName)
+//NewController creates new database controller with connection
+//databasePath - path to database
+func NewController(databasePath string) (*Sqlite3Controller, error) {
+	db, err := NewDB(databasePath)
 	if err != nil {
 		return nil, err
 	}
@@ -21,33 +24,233 @@ func NewController(databaseName string) (*Sqlite3Controller, error) {
 	return &result, nil
 }
 
+//AddUser add new user in database
 func (db *Sqlite3Controller) AddUser(user *storage.User) (int, error) {
-	stmt, err := db.Connection.Prepare("insert into Users(Name, TGId, NotificationHour, UTC) values (?, ?, ?, ?);")
-	if err != nil {
-		return 0, err
-	}
 	tx, err := db.Connection.Begin()
 	if err != nil {
-		return 0, err
+		return -1, err
 	}
-	result, err := tx.Stmt(stmt).Exec(user.Name, user.TGId, user.NotificationHour, user.UTC)
+	userId, err := addUser(user, tx)
 	if err != nil {
 		_ = tx.Rollback()
-		return 0, err
+		return -1, err
 	}
 	err = tx.Commit()
 	if err != nil {
-		return 0, err
+		return -1, err
+	}
+	user.Id = userId
+
+	return userId, nil
+}
+
+//addUser - add user processing, expected external transaction
+func addUser(user *storage.User, tx *sql.Tx) (int, error) {
+	stmt, err := tx.Prepare("insert into Users(Name, TGId, NotificationHour, UTC) values (?, ?, ?, ?);")
+	if err != nil {
+		return -1, err
+	}
+	result, err := tx.Stmt(stmt).Exec(user.Name, user.TGId, user.NotificationHour, user.UTC)
+	if err != nil {
+		return -1, err
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		return 0, err
+		return -1, err
 	}
-	user.Id = int(id)
 
 	return int(id), nil
 }
 
+//AddUserDomain - add tracked domain to user
+func (db *Sqlite3Controller) AddUserDomain(domain *storage.UserDomain) (bool, error) {
+	tx, err := db.Connection.Begin()
+	if err != nil {
+		return false, err
+	}
+	result, err := addUserDomain(domain, tx)
+	if err != nil {
+		_ = tx.Rollback()
+		return false, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return false, err
+	}
+
+	return result, nil
+}
+
+//addUserDomain - add tracked domain to user processing, expected external transaction
+func addUserDomain(domain *storage.UserDomain, tx *sql.Tx) (bool, error) {
+	stmt, err := tx.Prepare("insert into UserDomains(UserId, Domain) values (?, ?);")
+	if err != nil {
+		return false, err
+	}
+	result, err := tx.Stmt(stmt).Exec(domain.UserId, domain.Domain)
+	if err != nil {
+		return false, err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if affected > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+//RemoveUserDomain - remove tracked domain from user
+func (db *Sqlite3Controller) RemoveUserDomain(domain *storage.UserDomain) (bool, error) {
+	tx, err := db.Connection.Begin()
+	if err != nil {
+		return false, err
+	}
+	result, err := removeUserDomain(domain, tx)
+	if err != nil {
+		_ = tx.Rollback()
+		return false, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return false, err
+	}
+
+	return result, nil
+}
+
+//removeUserDomain - remove tracked domain from user, expected external transaction
+func removeUserDomain(domain *storage.UserDomain, tx *sql.Tx) (bool, error) {
+	stmt, err := tx.Prepare("delete from UserDomains where UserId = ? and Domain = ?;")
+	if err != nil {
+		return false, err
+	}
+	result, err := tx.Stmt(stmt).Exec(domain.UserId, domain.Domain)
+	if err != nil {
+		return false, err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if affected > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+//RemoveUser - remove user from database
+func (db *Sqlite3Controller) RemoveUser(user *storage.User) (bool, error) {
+	tx, err := db.Connection.Begin()
+	if err != nil {
+		return false, err
+	}
+	_, err = removeAllUserDomains(user, tx)
+	result, err := removeUser(user, tx)
+	if err != nil {
+		return false, err
+	}
+	if err != nil {
+		_ = tx.Rollback()
+		return false, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return false, err
+	}
+
+	return result, nil
+}
+
+//removeUser - remove user from database processing, expected external transaction
+func removeUser(user *storage.User, tx *sql.Tx) (bool, error) {
+	stmt, err := tx.Prepare("delete from Users where Id = ?;")
+	if err != nil {
+		return false, err
+	}
+	result, err := tx.Stmt(stmt).Exec(user.Id)
+	if err != nil {
+		return false, err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if affected > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+//removeAllUserDomains - remove all user domains from database processing, expected external transaction
+func removeAllUserDomains(user *storage.User, tx *sql.Tx) (bool, error) {
+	stmt, err := tx.Prepare("delete from UserDomains where UserId = ?;")
+	if err != nil {
+		return false, err
+	}
+	result, err := tx.Stmt(stmt).Exec(user.Id)
+	if err != nil {
+		return false, err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if affected > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+//UpdateUserInfo - updates user info in database
+func (db *Sqlite3Controller) UpdateUserInfo(user *storage.User) (bool, error) {
+	tx, err := db.Connection.Begin()
+	if err != nil {
+		return false, err
+	}
+	result, err := updateUserInfo(user, tx)
+	if err != nil {
+		return false, err
+	}
+	if err != nil {
+		_ = tx.Rollback()
+		return false, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return false, err
+	}
+
+	return result, nil
+}
+
+//updateUserInfo - updates user info in database processing, expected external transaction
+func updateUserInfo(user *storage.User, tx *sql.Tx) (bool, error) {
+	stmt, err := tx.Prepare("update Users" +
+		"	set Name = ?," +
+		"	TGId = ?," +
+		"	NotificationHour = ?," +
+		"	UTC = ?" +
+		"where Id = ?;")
+	if err != nil {
+		return false, err
+	}
+	result, err := tx.Stmt(stmt).Exec(user.Name, user.TGId, user.NotificationHour, user.UTC, user.Id)
+	if err != nil {
+		return false, err
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	if affected > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+//GetUserById - search user from database by id
 func (db *Sqlite3Controller) GetUserById(id int) (*storage.User, error) {
 	record, err := db.Connection.Query("select Id, Name, TGId, NotificationHour, UTC from Users where Id = ?;", id)
 	if err != nil {
@@ -69,6 +272,78 @@ func (db *Sqlite3Controller) GetUserById(id int) (*storage.User, error) {
 	return nil, fmt.Errorf("cannot find user by id %d", id)
 }
 
-func DisposeConnection(db *Sqlite3Controller) {
+//GetUserByTGId - search user from database by TGId
+func (db *Sqlite3Controller) GetUserByTGId(tgId string) (*storage.User, error) {
+	record, err := db.Connection.Query("select Id, Name, TGId, NotificationHour, UTC from Users where TGId = ?;", tgId)
+	if err != nil {
+		return nil, err
+	}
+	defer func(record *sql.Rows) {
+		_ = record.Close()
+	}(record)
+
+	if record.Next() {
+		var user storage.User
+		err := record.Scan(&user.Id, &user.Name, &user.TGId, &user.NotificationHour, &user.UTC)
+		if err != nil {
+			return nil, err
+		}
+		return &user, nil
+	}
+
+	return nil, fmt.Errorf("cannot find user by tgId %s", tgId)
+}
+
+//GetUserByName - search user from database by name
+func (db *Sqlite3Controller) GetUserByName(name string) (*storage.User, error) {
+	record, err := db.Connection.Query("select Id, Name, TGId, NotificationHour, UTC from Users where Name = ?;", name)
+	if err != nil {
+		return nil, err
+	}
+	defer func(record *sql.Rows) {
+		_ = record.Close()
+	}(record)
+
+	if record.Next() {
+		var user storage.User
+		err := record.Scan(&user.Id, &user.Name, &user.TGId, &user.NotificationHour, &user.UTC)
+		if err != nil {
+			return nil, err
+		}
+		return &user, nil
+	}
+
+	return nil, fmt.Errorf("cannot find user by Name %s", name)
+}
+
+//GetUserDomains - select user domains from database
+func (db *Sqlite3Controller) GetUserDomains(user *storage.User) (*[]storage.UserDomain, error) {
+	record, err := db.Connection.Query("select UserId, Domain from UserDomains where UserId = ?;", user.Id)
+	if err != nil {
+		return nil, err
+	}
+	defer func(record *sql.Rows) {
+		_ = record.Close()
+	}(record)
+
+	var userDomains []storage.UserDomain
+
+	for record.Next() {
+		var userDomain storage.UserDomain
+		err := record.Scan(&userDomain.UserId, &userDomain.Domain)
+		if err != nil {
+			return nil, err
+		}
+		userDomains = append(userDomains, userDomain)
+	}
+	if userDomains != nil {
+		return &userDomains, nil
+	}
+
+	return nil, fmt.Errorf("cannot find user domains for user %s", user.Name)
+}
+
+//DisposeController - close connections to database
+func DisposeController(db *Sqlite3Controller) {
 	CloseConnection(db.Connection)
 }
--- storage/sqlite3/controller_test.go
+++ storage/sqlite3/controller_test.go
@@ -2,6 +2,7 @@ package sqlite3
 
 import (
 	storage "certcheckerbot/storage"
+	"fmt"
 	"github.com/google/uuid"
 	"os"
 	"reflect"
@@ -41,7 +42,7 @@ func TestNewController(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := NewController(tt.args.databaseName)
-			defer DisposeConnection(got)
+			defer DisposeController(got)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewController() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -90,7 +91,7 @@ func TestSqlite3Controller_AddUser(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			controller, _ := NewController(dbName)
-			defer DisposeConnection(controller)
+			defer DisposeController(controller)
 
 			got, err := controller.AddUser(tt.args.user)
 			if (err != nil) != tt.wantErr {
@@ -112,6 +113,584 @@ func TestSqlite3Controller_AddUser(t *testing.T) {
 			}
 			if !reflect.DeepEqual(tt.args.user, user) {
 				t.Errorf("AddUser() selectd user afrer add in not equals with adding. Added = %v, selected = %v", tt.args.user, user)
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_GetUserByTGId(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	type args struct {
+		tgId string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *storage.User
+		wantErr bool
+	}{
+		{
+			name: "test GetUserByTGId",
+			args: args{
+				tgId: "test",
+			},
+			want: &storage.User{
+				Name:             "test",
+				TGId:             "test",
+				NotificationHour: 0,
+				UTC:              0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "test GetUserByTGId - fail to find",
+			args: args{
+				tgId: "test2",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+			if tt.want != nil {
+				tt.want.Id, _ = db.AddUser(tt.want)
+			}
+
+			got, err := db.GetUserByTGId(tt.args.tgId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetUserByTGId() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetUserByTGId() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_GetUserName(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *storage.User
+		wantErr bool
+	}{
+		{
+			name: "test GetUserByName",
+			args: args{
+				name: "test",
+			},
+			want: &storage.User{
+				Name:             "test",
+				TGId:             "test",
+				NotificationHour: 0,
+				UTC:              0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "test GetUserByName - fail to find",
+			args: args{
+				name: "test2",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+			if tt.want != nil {
+				tt.want.Id, _ = db.AddUser(tt.want)
+			}
+
+			got, err := db.GetUserByName(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetUserByName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetUserByName() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_AddUserDomain_FK_error(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	type args struct {
+		domain *storage.UserDomain
+	}
+	tests := []struct {
+		name              string
+		args              args
+		want              bool
+		wantErr           bool
+		expectedErrorText string
+	}{
+		{
+			name: "test AddUserDomain FK Error",
+			args: args{&storage.UserDomain{
+				UserId: 1,
+				Domain: "test.com",
+			}},
+			want:              false,
+			wantErr:           true,
+			expectedErrorText: "FOREIGN KEY constraint failed",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			got, err := db.AddUserDomain(tt.args.domain)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AddUserDomain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("AddUserDomain() got = %v, want %v", got, tt.want)
+				return
+			}
+			if err.Error() != tt.expectedErrorText {
+				t.Errorf("AddUserDomain() got = %v, want %v", err, tt.expectedErrorText)
+				return
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_AddUserDomain(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	type args struct {
+		domain *storage.UserDomain
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "test AddUserDomain correct",
+			args: args{&storage.UserDomain{
+				UserId: 1,
+				Domain: "test.com",
+			}},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			user := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			_, _ = db.AddUser(&user)
+			tt.args.domain.UserId = user.Id
+
+			got, err := db.AddUserDomain(tt.args.domain)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AddUserDomain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("AddUserDomain() got = %v, want %v", got, tt.want)
+				return
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_GetUserDomains(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	tests := []struct {
+		name    string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "test GetUserDomains correct",
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			user := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			_, _ = db.AddUser(&user)
+
+			domain := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test.com",
+			}
+			domain2 := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test2.ru",
+			}
+
+			var domains []storage.UserDomain
+
+			domains = append(domains, domain)
+			domains = append(domains, domain2)
+
+			for _, dom := range domains {
+				_, _ = db.AddUserDomain(&dom)
+			}
+
+			result, err := db.GetUserDomains(&user)
+			if err != nil {
+				t.Errorf("GetUserDomains() return error %v", err)
+				return
+			}
+			if !reflect.DeepEqual(result, &domains) {
+				t.Errorf("GetUserDomains() got %v, want %v", result, domains)
+				return
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_GetUserDomains_no_domains(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "test GetUserDomains no domains",
+			want: "cannot find user domains for user test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			user := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			_, err := db.GetUserDomains(&user)
+			if err == nil {
+				t.Errorf("GetUserDomains() expected error  %s", tt.want)
+				return
+			}
+			if err.Error() != tt.want {
+				t.Errorf("GetUserDomains()  got %v, want %v", err, tt.want)
+				return
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_RemoveUserDomain(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	tests := []struct {
+		name    string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "test RemoveUserDomain success",
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			user := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			_, _ = db.AddUser(&user)
+
+			domain := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test.com",
+			}
+			domain2 := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test2.ru",
+			}
+
+			var domains []storage.UserDomain
+
+			domains = append(domains, domain)
+			domains = append(domains, domain2)
+
+			for _, dom := range domains {
+				_, _ = db.AddUserDomain(&dom)
+			}
+
+			domains = domains[1:]
+
+			got, err := db.RemoveUserDomain(&domain)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RemoveUserDomain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("RemoveUserDomain() got = %v, want %v", got, tt.want)
+				return
+			}
+			result, err := db.GetUserDomains(&user)
+			if err != nil {
+				t.Errorf("RemoveUserDomain() return error %v", err)
+				return
+			}
+			if !reflect.DeepEqual(result, &domains) {
+				t.Errorf("RemoveUserDomain() got %v, want %v", result, domains)
+				return
+			}
+		})
+	}
+}
+
+func Test_removeAllUserDomains(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	tests := []struct {
+		name       string
+		want       bool
+		wantErr    bool
+		errMessage string
+	}{
+		{
+			name:       "test removeAllUserDomains success",
+			want:       true,
+			wantErr:    false,
+			errMessage: "cannot find user domains for user test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			user := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			_, _ = db.AddUser(&user)
+
+			domain := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test.com",
+			}
+			domain2 := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test2.ru",
+			}
+
+			var domains []storage.UserDomain
+
+			domains = append(domains, domain)
+			domains = append(domains, domain2)
+
+			for _, dom := range domains {
+				_, _ = db.AddUserDomain(&dom)
+			}
+
+			tx, _ := db.Connection.Begin()
+			got, err := removeAllUserDomains(&user, tx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("removeAllUserDomains() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			_ = tx.Commit()
+			if got != tt.want {
+				t.Errorf("removeAllUserDomains() got = %v, want %v", got, tt.want)
+				return
+			}
+			_, err = db.GetUserDomains(&user)
+			if err == nil {
+				t.Errorf("removeAllUserDomains() expected error %s", tt.errMessage)
+				return
+			}
+			if err.Error() != tt.errMessage {
+				t.Errorf("removeAllUserDomains() got = %v, want %s", err, tt.errMessage)
+				return
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_RemoveUser(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	tests := []struct {
+		name              string
+		want              bool
+		wantErr           bool
+		errMessageDomains string
+		errMessageUser    string
+	}{
+		{
+			name:              "test RemoveUser success",
+			want:              true,
+			wantErr:           false,
+			errMessageDomains: "cannot find user domains for user test",
+			errMessageUser:    "cannot find user by id",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			user := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			_, _ = db.AddUser(&user)
+
+			domain := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test.com",
+			}
+			domain2 := storage.UserDomain{
+				UserId: user.Id,
+				Domain: "test2.ru",
+			}
+
+			var domains []storage.UserDomain
+
+			domains = append(domains, domain)
+			domains = append(domains, domain2)
+
+			for _, dom := range domains {
+				_, _ = db.AddUserDomain(&dom)
+			}
+
+			got, err := db.RemoveUser(&user)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RemoveUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("RemoveUser() got = %v, want %v", got, tt.want)
+				return
+			}
+			_, err = db.GetUserDomains(&user)
+			if err == nil {
+				t.Errorf("RemoveUser() expected error %s", tt.errMessageDomains)
+				return
+			}
+			if err.Error() != tt.errMessageDomains {
+				t.Errorf("RemoveUser() got = %v, want %s", err, tt.errMessageDomains)
+				return
+			}
+			_, err = db.GetUserById(user.Id)
+			tt.errMessageUser = tt.errMessageUser + " " + fmt.Sprint(user.Id)
+			if err == nil {
+				t.Errorf("RemoveUser() expected error %s", tt.errMessageUser)
+				return
+			}
+			if err.Error() != tt.errMessageUser {
+				t.Errorf("RemoveUser() got = %v, want %s", err, tt.errMessageUser)
+				return
+			}
+		})
+	}
+}
+
+func TestSqlite3Controller_UpdateUserInfo(t *testing.T) {
+	dbName := getTempDBName()
+	defer removeDbFile(dbName)
+
+	tests := []struct {
+		name    string
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "test UpdateUserInfo success",
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, _ := NewController(dbName)
+			defer DisposeController(db)
+
+			userStart := storage.User{
+				Name: "test",
+				TGId: "test",
+			}
+
+			userChanged := storage.User{
+				Name:             "test new",
+				TGId:             "test new",
+				NotificationHour: 10,
+				UTC:              10,
+			}
+
+			_, _ = db.AddUser(&userStart)
+			userChanged.Id = userStart.Id
+
+			got, err := db.UpdateUserInfo(&userChanged)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UpdateUserInfo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("UpdateUserInfo() got = %v, want %v", got, tt.want)
+				return
+			}
+			dbUser, err := db.GetUserById(userStart.Id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UpdateUserInfo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(dbUser, &userChanged) {
+				t.Errorf("UpdateUserInfo() got %v, want %v", dbUser, userChanged)
+				return
 			}
 		})
 	}
